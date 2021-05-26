7122d96a79aded018591b7193bd716d103c11fd7
Pulsar Gupta <pulsar.d.gupta@gmail.com>
2021-05-26 19:05:16 +0530

    test1

--- src/main/java/com/runecast/demo/dao/ECInstanceDAO.java
+++ src/main/java/com/runecast/demo/dao/ECInstanceDAO.java
@@ -1,9 +1,11 @@
 package com.runecast.demo.dao;
 
-import com.runecast.demo.domainobject.ECInstanceDO;
+//import com.runecast.demo.domainobject.ECInstanceDO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ECInstanceDAO extends JpaRepository<ECInstanceDO,Long > {
+public interface ECInstanceDAO {//extends JpaRepository<ECInstanceDO,Long > {
 
-    ECInstanceDO findByEc2Id(String ec2Id);
+   // ECInstanceDO findByEc2Id(String ec2Id);
+    
+    //sdjhfjksdfkgh
 }
